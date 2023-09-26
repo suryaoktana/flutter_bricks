b72996f82aaccda3de0f4a30a8015afce0c7f8df
suryaoktana <suryaoktana@gmail.com>
2023-09-26 11:50:19 +0700

    remove repository on bloc bricks

--- bloc/__brick__/bloc/{{name.snakeCase()}}_bloc.dart
+++ bloc/__brick__/bloc/{{name.snakeCase()}}_bloc.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../{{name.snakeCase()}}.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part '{{name.snakeCase()}}_event.dart';
@@ -9,9 +8,8 @@ part '{{name.snakeCase()}}_state.dart';
 part '{{name.snakeCase()}}_bloc.freezed.dart';
 
 class {{name.pascalCase()}}Bloc extends Bloc<{{name.pascalCase()}}Event, {{name.pascalCase()}}State> {
-  {{name.pascalCase()}}Bloc({required {{name.pascalCase()}}Repository {{name.camelCase()}}Repository})
-      : _{{name.camelCase()}}Repository = {{name.camelCase()}}Repository,
-        super(const {{name.pascalCase()}}State()) {
+  {{name.pascalCase()}}Bloc()
+      : super(const {{name.pascalCase()}}State()) {
     on<{{name.pascalCase()}}Event>((events, emit) async {
       await events.map(
         fetch: (event) async => await _onFetch(event, emit),
